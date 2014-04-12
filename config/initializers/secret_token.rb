1ffe3fe73f5723d4a58f42f4e263e0ba55e65513
Nguyen Ngoc Tuan <dell@tuannn-pc.(none)>
2014-04-12 10:36:34 +0700

    initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nobitablog::Application.config.secret_key_base = '1b3c6f209636dc541857c9a36b995ca11a9f6793d3ebbbd3a3bc3368f3d369bdd1bffc932970074c8d739dae5c5f13e1fbe134c4604230d3f63642c7b6bdab3e'
